e1f7059228066ffe4eb95a7a44bc6c3b785da6fc
Ayumi01234 <ayumi.furukawa@mitsubishicorp.com>
2024-09-10 22:03:50 +0900

    Add files via upload

---
+++ gs_an_table.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: localhost
+-- 生成日時: 2024 年 9 月 10 日 15:02
+-- サーバのバージョン： 10.4.28-MariaDB
+-- PHP のバージョン: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `gs_db_class`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_an_table`
+--
+
+CREATE TABLE `gs_an_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `content` text NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `gs_an_table`
+--
+
+INSERT INTO `gs_an_table` (`id`, `name`, `email`, `content`, `date`) VALUES
+(2, '古川', 'ayumi@test.jp', '内容', '2024-07-11 19:39:36'),
+(3, 'yamada', 'yamada@test.jp', 'ご飯', '2024-07-11 19:40:10'),
+(4, 'Furukawa', 'kasti@gmail.com', 'お願い', '2024-07-11 20:38:23'),
+(5, 'Furukawa', 'kasti@gmail.com', 'お願い', '2024-07-11 20:38:32'),
+(6, 'ハリーポッター', 'jahs@amazo.com', '魔法が使えるようになりたい', '2024-07-13 22:28:35');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`),
+  ADD KEY `id_2` (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
